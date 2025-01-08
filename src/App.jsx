de4865248fd1528045f5076b19d6305151c30516
Abdulaziz31082010 <jamshidt234@gmail.com>
2025-01-08 16:54:08 +0500

    bot

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,12 @@ const App = () => {
   return (
     <div>
         <button onClick={handelClick}>click me</button>
+     <div className='img rasm'>
+      <img src="https://storage.kun.uz/source/3/Vl_i1uxggZlsLN-mhgv3xW480SDmDlCa.jpg" alt="" />
+      <button onClick={click}>click to view the mountain</button>
+     </div>
     </div>
   )
 }
 
-export default App
+export default App;
